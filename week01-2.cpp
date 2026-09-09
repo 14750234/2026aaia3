42f5d6f99c2f3cdb9742903095e23f1d993fa5b0
14750234 <14750234@me.mcu.edu.tw>
2026-09-09 11:05:52 +0800

    week01

---
+++ week01-2.cpp
@@ -0,0 +1,18 @@
+// week01-2.cpp
+#include <iostream> /// c++ 的輸入輸出,功能強大
+int main()
+{
+	int N;
+	std::cin >> N; /// c++ 輸入資料 標準::樹入 送到右邊
+	int b = N, ans = 0;
+	while(N>0){
+		ans = ans*10 + N%10;
+		N = N / 10;
+	}
+	/// c++ 輸入資料 將右邊的整數 依序送到左邊 送出
+	/// 錯 std::cout << b << ans << b+ans; //WRONG-ANSWER
+	///上面漏了 "+" 漏了"=" 又漏了跳行
+	std::cout << b << "+" << ans << "=" << b+ans << std::endl: /// 正確
+	std::cout << b << "+" << ans << "=" << b+ans << "\n"; /// 正確
+	printf("%d+%d=%d\n", b, ans, ans+b); /// 大一下交的竟然也能用
+}
