0e3bce321ce67ab78ccf726bec44438bc6988c07
14750234 <14750234@me.mcu.edu.tw>
2026-09-16 11:39:49 +0800

    week02

---
+++ week02/week02-1.cpp
@@ -0,0 +1,13 @@
+/// week02-1.cpp 練習 c++ string 字串
+/// File-Save As 存檔成 .cpp檔
+#include <iostream> /// 上週交的
+#include <string> /// 今天新教的
+
+int main()
+{
+    std::cout << "請輸入你的名字: ";
+    std::string name; /// 使用標準的字串
+    std::cin >> name;
+    std::cout << name << "你好,你會用字串了";
+}
+
